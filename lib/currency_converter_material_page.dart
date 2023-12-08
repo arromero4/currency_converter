6b62b7c2b9d857f781808714ddce857aacc5aad0
arromero4 <arromero0408@gmail.com>
2023-12-08 02:44:17 -0600

    Modifying currency_converter_material_page adding padding

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,102 +1,56 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
-  ///Widget requiere algo conocido como key para su Constructor
-  ///donde sea que extiendas una clase abstracta tienes que pasar key a la superclase
   const CurrencyConverterMaterialPage({super.key});
-  //({Key? key : super(key: key)});
 
   @override
-
-  ///BuildContext is basically a telling
-  ///the location of the widget
-  ///in the widget tree
   Widget build(BuildContext context) {
     final border = OutlineInputBorder(
+      //Color(0xFFAARRGGBB)
       borderSide: const BorderSide(
-          color: Colors.white,
-          width: 2,
-          style: BorderStyle.solid,
-          strokeAlign: BorderSide.strokeAlignOutside),
+        color: Colors.black,
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
       borderRadius: BorderRadius.circular(10),
     );
 
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(20, 40, 52, 1),
+      backgroundColor: const Color.fromRGBO(74, 74, 74, 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              '0',
-              style: TextStyle(
-                  fontSize: 35, //default 14
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(9, 150, 217, 1)),
-            ),
-            //padding
-            //container
-            Padding( 
+            const Text('0',
+                style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(255, 255, 255, 1))),
+            Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
-                style: const TextStyle(color: Colors.white),
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
                 decoration: InputDecoration(
-                  hintText: 'Please enter the amount in USD:',
+                  hintText: 'Please enter the amount in USD: ',
                   hintStyle: const TextStyle(
-                    color: Colors.white,
+                    color: Colors.black,
                   ),
-                  prefixIcon: const Icon(Icons.monetization_on),
-                  prefixIconColor: Colors.lightBlue,
+                  prefixIcon: const Icon(
+                    Icons.monetization_on,
+                  ),
+                  prefixIconColor: Colors.black,
+                  filled: true,
                   fillColor: Colors.white,
                   focusedBorder: border,
                   enabledBorder: border,
                 ),
-                keyboardType:
-                    const TextInputType.numberWithOptions(decimal: true),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
               ),
             ),
-            //buttons:
-            //1. raised
-            //2. appears like a text
-<<<<<<< HEAD
-            TextButton(
-              onPressed: () {
-                //debug, release, profile
-                //to run in release mode
-                //flutter run -release or -profile
-                if (kDebugMode) {
-                  debugPrint('clic');
-                }
-              },
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue[700],
-                foregroundColor: Colors.white,
-                fixedSize: const Size(150, 50),
-=======
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  //debug, release, profile
-                  //to run in release mode
-                  //flutter run -release or -profile
-                  if (kDebugMode) {
-                    debugPrint('clic');
-                  }
-                },
-                style: ButtonStyle(
-                  elevation: const MaterialStatePropertyAll(15),
-                  backgroundColor: MaterialStatePropertyAll(Colors.blue[700]),
-                  foregroundColor: const MaterialStatePropertyAll(Colors.white),
-                  minimumSize: const MaterialStatePropertyAll(
-                    Size(double.infinity, 50),
-                  ),
-                ),
-                child: const Text('Convert'),
->>>>>>> 522ddb1af2c3f11aef4e2b560654c136583c0ecf
-              ),
-            )
           ],
         ),
       ),
